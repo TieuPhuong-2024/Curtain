fee40f745671a2bbf1b9c32335f5ca04ce0b41a6
TieuPhuong-2024 <thamphuong.crochet@gmail.com>
2025-04-19 00:34:53 +0700

    Add category management features, including APIs for creating, updating, and deleting categories. Update corresponding routes in `category.routes.js`.

--- backend/src/routes/category.routes.js
+++ backend/src/routes/category.routes.js
@@ -4,4 +4,13 @@ const categoryController = require('../controllers/category.controller');
 
 router.get('/', categoryController.getCategories);
 
+// POST create a new category
+router.post('/', categoryController.createCategory);
+
+// PUT update a category
+router.put('/:id', categoryController.updateCategory);
+
+// DELETE a category
+router.delete('/:id', categoryController.deleteCategory);
+
 module.exports = router;
